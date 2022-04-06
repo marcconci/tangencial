9272516aaafa8ef186b6bcc7515ac83912f11cf6
ocondez <marcosconci@live.com>
2022-04-06 10:52:05 -0300

    1.0

---
+++ main.py
@@ -0,0 +1,8 @@
+#Cálculo de áreas
+
+def area(p):
+    return 0.5 * abs(sum(x0*y1 - x1*y0
+                         for ((x0, y0), (x1, y1)) in segments(p)))
+
+def segments(p):
+    return zip(p, p[1:] + [p[0]])
